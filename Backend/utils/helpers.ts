ff5c2a528ca8550649250ed2c1046decb091d6dc
RehmanAli10 <vrehmanali@gmail.com>
2026-02-18 23:52:24 +0500

    update logic to get slots for 14 days

--- Backend/utils/helpers.ts
+++ Backend/utils/helpers.ts
@@ -1,11 +1,19 @@
-export function getDateRange(daysFromToday: number = 7): {
+export function getDateRange(
+  daysFromToday: number = 7,
+  excludeToday: boolean = false,
+): {
   start: string;
   end: string;
 } {
   const start = new Date();
+
+  if (excludeToday) {
+    start.setDate(start.getDate() + 1);
+  }
+
   start.setHours(0, 0, 0, 0);
 
-  const end = new Date();
+  const end = new Date(start);
   end.setDate(start.getDate() + daysFromToday);
   end.setHours(23, 59, 59, 999);
 
